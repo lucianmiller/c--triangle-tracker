c5dcaead2210873bff3076ee1dc6475e82a542af
Lucian Miller <lucian.miller185@gmail.com>
2022-07-29 09:36:44 -0700

    Add tests for CheckType function

--- TriangleTracker.Solution/TriangleTrackerTests/ModelsTest/TrackerTest.cs
+++ TriangleTracker.Solution/TriangleTrackerTests/ModelsTest/TrackerTest.cs
@@ -10,9 +10,51 @@ namespace Tracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(1, 2, 3);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
+    [TestMethod]
+    public void TriangleConstructor_StoresValuesCorrectly_Triangle()
+    {
+      //Arrange
+      Triangle newTriangle = new Triangle(2, 2, 8);
+
+      //Assert
+      Assert.AreEqual(2, newTriangle.Side1);
+      Assert.AreEqual(2, newTriangle.Side2);
+      Assert.AreEqual(8, newTriangle.Side3);
+      Assert.AreEqual("", newTriangle.Type);
+    }
+
+    [TestMethod]
+    public void CheckType_DeterminesNotATriangle_Triangle()
+    {
+      //Arrange
+      Triangle newTriangle = new Triangle(2, 2, 8);
+      newTriangle.CheckType();
+
+      //Act
+      string answer = "Not a triangle";
+      string result = newTriangle.Type;
+
+      //Assert
+      Assert.AreEqual(answer, result);
+    }
+
+    [TestMethod]
+    public void CheckType_DeterminesEquilateralTriangle_Triangle()
+    {
+      //Arrange
+      Triangle newTriangle = new Triangle(3, 3, 3);
+      newTriangle.CheckType();
+
+      //Act
+      string answer = "Equilateral Triangle";
+      string result = newTriangle.Type;
+
+      //Assert
+      Assert.AreEqual(answer, result);
+    }
   }
 }
