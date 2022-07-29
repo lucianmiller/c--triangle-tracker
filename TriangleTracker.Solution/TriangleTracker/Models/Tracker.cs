f15156ff136bc88b376a2a133c738cce9ec071fa
Lucian Miller <lucian.miller185@gmail.com>
2022-07-29 09:36:58 -0700

    Add ability to check if not a triangle

--- TriangleTracker.Solution/TriangleTracker/Models/Tracker.cs
+++ TriangleTracker.Solution/TriangleTracker/Models/Tracker.cs
@@ -2,6 +2,26 @@ namespace Tracker.Models
 {
   public class Triangle
   {
-    
+    //Public class properties
+    public int Side1 { get; set; }
+    public int Side2 { get; set; }
+    public int Side3 { get; set; }
+    public string Type { get; set;}
+
+    //Constructor: Assigns values to properties
+    public Triangle(int side1, int side2, int side3)
+    {
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
+      Type = "";
+    }
+
+    public void CheckType()
+    {
+      if (this.Side1 + this.Side2 <= this.Side3 || this.Side1 + this.Side3 <= this.Side2 || this.Side2 + this.Side3 <= this.Side1) {
+        this.Type = "Not a triangle";
+      }
+    }
   }
 }
